f7e3ee6880063c5dedcbcb576befcc7791eb0936
nic-jol <njlefebvre@gmail.com>
2018-03-19 19:57:50 -0600

    Order and Payment Tabs Done

--- OrderTab/WpfApplication2/MainWindow.xaml.cs
+++ OrderTab/WpfApplication2/MainWindow.xaml.cs
@@ -30,9 +30,14 @@ namespace WpfApplication2
             Table4_O.tableNum.Text = "23";
 
             Table1_O.seatsFilled.Text = "0/4";
-            Table2_O.seatsFilled.Text = "0/4";
-            Table3_O.seatsFilled.Text = "0/4";
-            Table4_O.seatsFilled.Text = "0/4";
+            Table2_O.seatsFilled.Text = "R-2";
+            Table3_O.seatsFilled.Text = "2/4";
+            Table4_O.seatsFilled.Text = "4/4";
+
+            Table1_P.tableNum.Text = "20";
+            Table2_P.tableNum.Text = "21";
+            Table3_P.tableNum.Text = "22";
+            Table4_P.tableNum.Text = "23";
         }
     }
 }
