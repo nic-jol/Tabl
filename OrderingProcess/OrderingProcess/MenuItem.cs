6db6fd742a1e5bb148ff7832c39e2b56cb028e24
nic-jol <njlefebvre@gmail.com>
2018-04-08 20:22:03 -0600

    Added Menu Item Class

---
+++ OrderingProcess/OrderingProcess/MenuItem.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingProcess
+{
+    public class MenuItem
+    {
+        //Attributes
+        string name;
+        int size;
+        string side;
+        List<string> validMenuItem = new List<string>();
+        List<string> validSide = new List<string>();
+
+        public MenuItem()
+        {
+            name = null;
+            size = -1;
+            side = null;
+            buildValidMenu();
+            buildValidSides();
+        }
+
+        public MenuItem(string newName)
+        {
+            name = newName;
+            size = -1;
+            side = null;
+            buildValidMenu();
+            buildValidSides();
+        }
+
+        public MenuItem(string newName, int newSize, string newSide)
+        {
+            name = newName;
+            size = newSize;
+            side = newSide;
+            buildValidMenu();
+            buildValidSides();
+        }
+
+        private void buildValidMenu()
+        {
+            // Food
+            validMenuItem.Add("Rotisserie Chicken");
+            validMenuItem.Add("Tuna Sandwich");
+            validMenuItem.Add("Steak");
+            validMenuItem.Add("Margherita Pizza");
+            validMenuItem.Add("Classic Burger");
+
+            // Drinks
+            validMenuItem.Add("Coke");
+            validMenuItem.Add("Orange Crush");
+            validMenuItem.Add("Sprite");
+            validMenuItem.Add("Milk");
+            validMenuItem.Add("Coffee");
+        }
+
+        private void buildValidSides()
+        {
+            validSide.Add("Regular Fries");
+            validSide.Add("Sweet Potato Fries");
+            validSide.Add("Garden Salad");
+            validSide.Add("Caesar Salad");
+        }
+
+
+
+        public void setName(string newName)
+        {
+            if (validMenuItem.Contains(newName))
+            {
+                name = newName;
+            }
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public void setSide(string newSide)
+        {
+            if (validSide.Contains(newSide))
+            {
+                side = newSide;
+            }
+        }
+
+        public string getSide()
+        {
+            return side;
+        }
+
+        public void setSize(int newSize)
+        {
+            if ((newSize >= 0) && (newSize < 3))
+            {
+                size = newSize;
+            }
+        }
+
+        public int getSize()
+        {
+            return size;
+        }
+    }
+}
